12258669442d0f3fcb87737bac042acd4f685ecf
Dominik140396 <113764770+Dominik140396@users.noreply.github.com>
2022-11-16 13:37:02 +0100

    New game

--- Schere, Stein, Papier/logic.js
+++ Schere, Stein, Papier/logic.js
@@ -17,7 +17,7 @@ function setRound() {
 function game(userChoice) {
     let result;
     function getRandomInt() {
-        return Math.floor(Math.random() * 4);
+        return Math.floor(Math.random() * 5);
     }
     let compChoice = getRandomInt();
 
@@ -25,7 +25,8 @@ function game(userChoice) {
         case 0: compChoice = "Schere"; break;
         case 1: compChoice = "Stein"; break;
         case 2: compChoice = "Papier"; break;
-        case 3: compChoice = "Brunnen"; break;
+        case 3: compChoice = "Echse"; break;
+        case 4: compChoice = "Spock"; break;
     }
     // 0 Unentschieden, 1 Sieg, 2 Lose
         if(compChoice === "Schere") {
@@ -33,7 +34,9 @@ function game(userChoice) {
                 case "Schere": result = 0; break;
                 case "Stein": result = 1; break;
                 case "Papier": result = 2; break;
-                case "Brunnen": result = 1; break;
+                case "Echse": result = 2; break;
+                case "Spock": result = 1; break;
+
             }
         }
     if(compChoice === "Stein") {
@@ -41,7 +44,8 @@ function game(userChoice) {
             case "Schere": result = 2; break;
             case "Stein": result = 0; break;
             case "Papier": result = 1; break;
-            case "Brunnen": result = 1; break;
+            case "Echse": result = 2; break;
+            case "Spock": result = 1; break;
         }
     }
     if(compChoice === "Papier") {
@@ -49,15 +53,26 @@ function game(userChoice) {
             case "Schere": result = 1; break; //gewinnt User
             case "Stein": result = 2; break; //verliert User
             case "Papier": result = 0; break; //Unentschieden
-            case "Brunnen": result = 2; break;
+            case "Echse": result = 1; break;
+            case "Spock": result = 2; break;
         }
     }
-    if(compChoice === "Brunnen") {
+    if(compChoice === "Echse") {
+        switch (userChoice) {
+            case "Schere": result = 1; break;
+            case "Stein": result = 1; break;
+            case "Papier": result = 2; break;
+            case "Echse": result = 0; break;
+            case "Spock": result = 2; break;
+        }
+    }
+    if(compChoice === "Spock") {
         switch (userChoice) {
             case "Schere": result = 2; break;
             case "Stein": result = 2; break;
             case "Papier": result = 1; break;
-            case "Brunnen": result = 0; break;
+            case "Echse": result = 1; break;
+            case "Spock": result = 0; break;
         }
     }
     rounds++;
@@ -78,7 +93,7 @@ function game(userChoice) {
 
     if(rounds === Number(maxRounds)) {
         if (userChoice > compChoice) {
-            document.querySelector('.game').innerHTML = `<div class="gameoverText">Spiel ist vorbei, User hat gewonnen<br> ${NewGame}</div>`;
+            document.querySelector('.game').innerHTML = `<div class="gameoverText">Spiel ist vorbei, Du hat gewonnen<br> ${NewGame}</div>`;
         } else if (compWin > userWin) {
             document.querySelector('.game').innerHTML = `<div class="gameoverText">Spiel ist vorbei, NPC hat gewonnen<br> ${NewGame}</div>`;
         } else {
